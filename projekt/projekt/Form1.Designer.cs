8a7e27ac1b1bad6dafbfed767d4d714c7cd8595b
JakubM61893 <84183330+JakubM61893@users.noreply.github.com>
2021-07-03 13:52:19 +0200

    Add files via upload

---
+++ projekt/projekt/Form1.Designer.cs
@@ -0,0 +1,354 @@
+﻿
+namespace projekt
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.pictureBox5 = new System.Windows.Forms.PictureBox();
+            this.pictureBox6 = new System.Windows.Forms.PictureBox();
+            this.pictureBox7 = new System.Windows.Forms.PictureBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.bolid = new System.Windows.Forms.PictureBox();
+            this.pictureBox9 = new System.Windows.Forms.PictureBox();
+            this.dziura1 = new System.Windows.Forms.PictureBox();
+            this.dziura2 = new System.Windows.Forms.PictureBox();
+            this.dziura3 = new System.Windows.Forms.PictureBox();
+            this.koniec = new System.Windows.Forms.Label();
+            this.dolar1 = new System.Windows.Forms.PictureBox();
+            this.dolar3 = new System.Windows.Forms.PictureBox();
+            this.dolar2 = new System.Windows.Forms.PictureBox();
+            this.dolar4 = new System.Windows.Forms.PictureBox();
+            this.coins = new System.Windows.Forms.Label();
+            this.exit = new System.Windows.Forms.Button();
+            this.restart = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox7)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bolid)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox9)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dziura1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dziura2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dziura3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dolar1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dolar3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dolar2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dolar4)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.White;
+            this.pictureBox1.Location = new System.Drawing.Point(223, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(10, 72);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackColor = System.Drawing.Color.White;
+            this.pictureBox2.Location = new System.Drawing.Point(223, 109);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(10, 72);
+            this.pictureBox2.TabIndex = 1;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.BackColor = System.Drawing.Color.White;
+            this.pictureBox3.Location = new System.Drawing.Point(223, 208);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(10, 72);
+            this.pictureBox3.TabIndex = 2;
+            this.pictureBox3.TabStop = false;
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.BackColor = System.Drawing.Color.White;
+            this.pictureBox4.Location = new System.Drawing.Point(223, 310);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(10, 72);
+            this.pictureBox4.TabIndex = 3;
+            this.pictureBox4.TabStop = false;
+            // 
+            // pictureBox5
+            // 
+            this.pictureBox5.BackColor = System.Drawing.Color.White;
+            this.pictureBox5.Location = new System.Drawing.Point(223, 412);
+            this.pictureBox5.Name = "pictureBox5";
+            this.pictureBox5.Size = new System.Drawing.Size(10, 72);
+            this.pictureBox5.TabIndex = 4;
+            this.pictureBox5.TabStop = false;
+            // 
+            // pictureBox6
+            // 
+            this.pictureBox6.BackColor = System.Drawing.Color.White;
+            this.pictureBox6.Location = new System.Drawing.Point(0, 1);
+            this.pictureBox6.Name = "pictureBox6";
+            this.pictureBox6.Size = new System.Drawing.Size(10, 617);
+            this.pictureBox6.TabIndex = 5;
+            this.pictureBox6.TabStop = false;
+            // 
+            // pictureBox7
+            // 
+            this.pictureBox7.BackColor = System.Drawing.Color.White;
+            this.pictureBox7.Location = new System.Drawing.Point(473, 1);
+            this.pictureBox7.Name = "pictureBox7";
+            this.pictureBox7.Size = new System.Drawing.Size(10, 617);
+            this.pictureBox7.TabIndex = 6;
+            this.pictureBox7.TabStop = false;
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 10;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // bolid
+            // 
+            this.bolid.Image = ((System.Drawing.Image)(resources.GetObject("bolid.Image")));
+            this.bolid.Location = new System.Drawing.Point(101, 390);
+            this.bolid.Name = "bolid";
+            this.bolid.Size = new System.Drawing.Size(64, 176);
+            this.bolid.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.bolid.TabIndex = 7;
+            this.bolid.TabStop = false;
+            // 
+            // pictureBox9
+            // 
+            this.pictureBox9.BackColor = System.Drawing.Color.White;
+            this.pictureBox9.Location = new System.Drawing.Point(223, 511);
+            this.pictureBox9.Name = "pictureBox9";
+            this.pictureBox9.Size = new System.Drawing.Size(10, 72);
+            this.pictureBox9.TabIndex = 8;
+            this.pictureBox9.TabStop = false;
+            // 
+            // dziura1
+            // 
+            this.dziura1.Image = ((System.Drawing.Image)(resources.GetObject("dziura1.Image")));
+            this.dziura1.Location = new System.Drawing.Point(40, 154);
+            this.dziura1.Name = "dziura1";
+            this.dziura1.Size = new System.Drawing.Size(56, 52);
+            this.dziura1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.dziura1.TabIndex = 9;
+            this.dziura1.TabStop = false;
+            // 
+            // dziura2
+            // 
+            this.dziura2.Image = ((System.Drawing.Image)(resources.GetObject("dziura2.Image")));
+            this.dziura2.Location = new System.Drawing.Point(354, 168);
+            this.dziura2.Name = "dziura2";
+            this.dziura2.Size = new System.Drawing.Size(59, 56);
+            this.dziura2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.dziura2.TabIndex = 10;
+            this.dziura2.TabStop = false;
+            // 
+            // dziura3
+            // 
+            this.dziura3.Image = ((System.Drawing.Image)(resources.GetObject("dziura3.Image")));
+            this.dziura3.Location = new System.Drawing.Point(294, 368);
+            this.dziura3.Name = "dziura3";
+            this.dziura3.Size = new System.Drawing.Size(59, 56);
+            this.dziura3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.dziura3.TabIndex = 11;
+            this.dziura3.TabStop = false;
+            // 
+            // koniec
+            // 
+            this.koniec.AutoSize = true;
+            this.koniec.BackColor = System.Drawing.Color.Gold;
+            this.koniec.Font = new System.Drawing.Font("Showcard Gothic", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.koniec.ForeColor = System.Drawing.Color.Red;
+            this.koniec.Location = new System.Drawing.Point(123, 267);
+            this.koniec.Name = "koniec";
+            this.koniec.Size = new System.Drawing.Size(230, 40);
+            this.koniec.TabIndex = 12;
+            this.koniec.Text = "Koniec Gry!";
+            // 
+            // dolar1
+            // 
+            this.dolar1.Image = ((System.Drawing.Image)(resources.GetObject("dolar1.Image")));
+            this.dolar1.Location = new System.Drawing.Point(51, 332);
+            this.dolar1.Name = "dolar1";
+            this.dolar1.Size = new System.Drawing.Size(30, 30);
+            this.dolar1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.dolar1.TabIndex = 13;
+            this.dolar1.TabStop = false;
+            // 
+            // dolar3
+            // 
+            this.dolar3.Image = ((System.Drawing.Image)(resources.GetObject("dolar3.Image")));
+            this.dolar3.Location = new System.Drawing.Point(258, 68);
+            this.dolar3.Name = "dolar3";
+            this.dolar3.Size = new System.Drawing.Size(30, 30);
+            this.dolar3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.dolar3.TabIndex = 14;
+            this.dolar3.TabStop = false;
+            // 
+            // dolar2
+            // 
+            this.dolar2.Image = ((System.Drawing.Image)(resources.GetObject("dolar2.Image")));
+            this.dolar2.Location = new System.Drawing.Point(178, 356);
+            this.dolar2.Name = "dolar2";
+            this.dolar2.Size = new System.Drawing.Size(30, 30);
+            this.dolar2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.dolar2.TabIndex = 15;
+            this.dolar2.TabStop = false;
+            // 
+            // dolar4
+            // 
+            this.dolar4.Image = ((System.Drawing.Image)(resources.GetObject("dolar4.Image")));
+            this.dolar4.Location = new System.Drawing.Point(433, 78);
+            this.dolar4.Name = "dolar4";
+            this.dolar4.Size = new System.Drawing.Size(30, 30);
+            this.dolar4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.dolar4.TabIndex = 16;
+            this.dolar4.TabStop = false;
+            // 
+            // coins
+            // 
+            this.coins.AutoSize = true;
+            this.coins.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.coins.ForeColor = System.Drawing.Color.Blue;
+            this.coins.Location = new System.Drawing.Point(17, 10);
+            this.coins.Name = "coins";
+            this.coins.Size = new System.Drawing.Size(85, 25);
+            this.coins.TabIndex = 17;
+            this.coins.Text = "Coins=0";
+            // 
+            // exit
+            // 
+            this.exit.BackColor = System.Drawing.Color.Red;
+            this.exit.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.exit.Location = new System.Drawing.Point(258, 363);
+            this.exit.Name = "exit";
+            this.exit.Size = new System.Drawing.Size(214, 86);
+            this.exit.TabIndex = 18;
+            this.exit.Text = "Wyjście";
+            this.exit.UseVisualStyleBackColor = false;
+            this.exit.Click += new System.EventHandler(this.exit_Click);
+            // 
+            // restart
+            // 
+            this.restart.BackColor = System.Drawing.Color.LightCoral;
+            this.restart.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.restart.ForeColor = System.Drawing.Color.Green;
+            this.restart.Location = new System.Drawing.Point(12, 363);
+            this.restart.Name = "restart";
+            this.restart.Size = new System.Drawing.Size(214, 86);
+            this.restart.TabIndex = 19;
+            this.restart.Text = "Spróbuj ponownie";
+            this.restart.UseVisualStyleBackColor = false;
+            this.restart.Click += new System.EventHandler(this.restart_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Gray;
+            this.ClientSize = new System.Drawing.Size(484, 611);
+            this.Controls.Add(this.restart);
+            this.Controls.Add(this.exit);
+            this.Controls.Add(this.coins);
+            this.Controls.Add(this.dolar4);
+            this.Controls.Add(this.dolar2);
+            this.Controls.Add(this.dolar3);
+            this.Controls.Add(this.dolar1);
+            this.Controls.Add(this.koniec);
+            this.Controls.Add(this.dziura3);
+            this.Controls.Add(this.dziura2);
+            this.Controls.Add(this.dziura1);
+            this.Controls.Add(this.pictureBox9);
+            this.Controls.Add(this.bolid);
+            this.Controls.Add(this.pictureBox7);
+            this.Controls.Add(this.pictureBox6);
+            this.Controls.Add(this.pictureBox5);
+            this.Controls.Add(this.pictureBox4);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox7)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bolid)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox9)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dziura1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dziura2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dziura3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dolar1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dolar3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dolar2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dolar4)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.PictureBox pictureBox4;
+        private System.Windows.Forms.PictureBox pictureBox5;
+        private System.Windows.Forms.PictureBox pictureBox6;
+        private System.Windows.Forms.PictureBox pictureBox7;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.PictureBox bolid;
+        private System.Windows.Forms.PictureBox pictureBox9;
+        private System.Windows.Forms.PictureBox dziura1;
+        private System.Windows.Forms.PictureBox dziura2;
+        private System.Windows.Forms.PictureBox dziura3;
+        private System.Windows.Forms.Label koniec;
+        private System.Windows.Forms.PictureBox dolar1;
+        private System.Windows.Forms.PictureBox dolar3;
+        private System.Windows.Forms.PictureBox dolar2;
+        private System.Windows.Forms.PictureBox dolar4;
+        private System.Windows.Forms.Label coins;
+        private System.Windows.Forms.Button exit;
+        private System.Windows.Forms.Button restart;
+    }
+}
+
---
+++ projekt/projekt/Form1.cs
@@ -0,0 +1,260 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace projekt
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            koniec.Visible = false; //przy rozpoczeciu gry niewidzialne
+            restart.Visible = false;
+            exit.Visible = false;
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            poruszanie_tla(5);
+            przeszkody(5);
+            koniecgry();
+            dolary(5);
+            dolaryzbierane();
+        }
+
+        int dolaryzebrane = 0;
+
+        Random r = new Random();
+        int x;
+        void przeszkody(int predkosc)
+        {
+            if (dziura1.Top >= 500)
+            {
+                x = r.Next(0, 150);
+
+                dziura1.Location = new Point(x, 0);
+            }
+            else
+            {
+                dziura1.Top += predkosc;
+            }
+
+            if (dziura2.Top >= 500)
+            {
+                x = r.Next(160, 300);
+                dziura2.Location = new Point(x, 0);
+            }
+            else
+            {
+                dziura2.Top += predkosc;
+            }
+            if(dziura3.Top >= 500)
+            {
+                x = r.Next(300, 480);
+                dziura3.Location = new Point(x, 0);
+            }
+            else
+            {
+                dziura3.Top += predkosc;
+            }
+        }
+
+        void dolary(int predkosc)
+        {
+            if (dolar1.Top >= 500)
+            {
+                x = r.Next(10, 130);
+
+                dolar1.Location = new Point(x, 0);
+            }
+            else
+            {
+                dolar1.Top += predkosc;
+            }
+
+            if (dolar2.Top >= 500)
+            {
+                x = r.Next(100, 280);
+                dolar2.Location = new Point(x, 0);
+            }
+            else
+            {
+                dolar2.Top += predkosc;
+            }
+            if (dolar3.Top >= 500)
+            {
+                x = r.Next(250, 360);
+                dolar3.Location = new Point(x, 0);
+            }
+            else
+            {
+                dolar3.Top += predkosc;
+            }
+            if (dolar4.Top >= 500)
+            {
+                x = r.Next(300, 470);
+                dolar4.Location = new Point(x, 0);
+            }
+            else
+            {
+                dolar4.Top += predkosc;
+            }
+        }
+
+        void koniecgry()
+        {
+            if(bolid.Bounds.IntersectsWith(dziura1.Bounds))
+            {
+                timer1.Enabled = false;
+                koniec.Visible = true;
+                restart.Visible = true;
+                exit.Visible = true;
+            }
+            if (bolid.Bounds.IntersectsWith(dziura2.Bounds))
+            {
+                timer1.Enabled = false;
+                koniec.Visible = true;
+                restart.Visible = true;
+                exit.Visible = true;
+            }
+            if (bolid.Bounds.IntersectsWith(dziura3.Bounds))
+            {
+                timer1.Enabled = false;
+                koniec.Visible = true;
+                restart.Visible = true;
+                exit.Visible = true;
+            }
+        }
+
+        void poruszanie_tla(int predkosc)
+        {
+            if (pictureBox1.Top >= 500)
+            {
+                pictureBox1.Top = 0;
+            }
+            else
+            {
+                pictureBox1.Top += predkosc;
+            }
+
+            if (pictureBox2.Top >= 500)
+            {
+                pictureBox2.Top = 0;
+            }
+            else
+            {
+                pictureBox2.Top += predkosc;
+            }
+
+            if (pictureBox3.Top >= 500)
+            {
+                pictureBox3.Top = 0;
+            }
+            else
+            {
+                pictureBox3.Top += predkosc;
+            }
+
+            if (pictureBox4.Top >= 500)
+            {
+                pictureBox4.Top = 0;
+            }
+            else
+            {
+                pictureBox4.Top += predkosc;
+            }
+
+            if (pictureBox5.Top >= 500)
+            {
+                pictureBox5.Top = 0;
+            }
+            else
+            {
+                pictureBox5.Top += predkosc;
+            }
+
+            if (pictureBox9.Top >= 500)
+            {
+                pictureBox9.Top = 0;
+            }
+            else
+            {
+                pictureBox9.Top += predkosc;
+            }
+        }
+
+        void dolaryzbierane()
+        {
+            if(bolid.Bounds.IntersectsWith(dolar1.Bounds))
+            {
+                dolaryzebrane++;
+                coins.Text = "Coins=" + dolaryzebrane.ToString();
+                x = r.Next(20, 130);
+
+                dolar1.Location = new Point(x, 0);
+            }
+            if (bolid.Bounds.IntersectsWith(dolar2.Bounds))
+            {
+                dolaryzebrane++;
+                coins.Text = "Coins=" + dolaryzebrane.ToString();
+                x = r.Next(100, 280);
+                dolar2.Location = new Point(x, 0);
+            }
+            if (bolid.Bounds.IntersectsWith(dolar3.Bounds))
+            {
+                dolaryzebrane++;
+                coins.Text = "Coins=" + dolaryzebrane.ToString();
+                x = r.Next(250, 360);
+                dolar3.Location = new Point(x, 0);
+            }
+            if (bolid.Bounds.IntersectsWith(dolar4.Bounds))
+            {
+                dolaryzebrane++;
+                coins.Text = "Coins=" + dolaryzebrane.ToString();
+                x = r.Next(300, 470);
+                dolar4.Location = new Point(x, 0);
+            }
+        }
+        
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Left)
+            {
+                if(bolid.Left>20) //ograniczenie poruszania z lewej strony
+                bolid.Left += -10;
+            }
+            if (e.KeyCode == Keys.Right)
+            {
+                if(bolid.Right<470) //ograniczenie poruszania z prawej strony
+                bolid.Left += 10;
+            }
+            if (e.KeyCode == Keys.Up)
+            {
+                if (bolid.Top > 10)
+                    bolid.Top += -10;
+            }
+            if (e.KeyCode == Keys.Down)
+            {
+                if (bolid.Top < 440)
+                    bolid.Top += 10;
+            }
+        }
+
+        private void restart_Click(object sender, EventArgs e)
+        {
+            Application.Restart();
+        }
+
+        private void exit_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
